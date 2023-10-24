73111421f1c13bd612cbdaad79c4fae85563b1db
Mikhail Rumyantsev <mi.rumyantsev.2020@gmail.com>
2023-10-24 21:45:43 +0000

    Add feature to get version

--- cmd/passgen/main.go
+++ cmd/passgen/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"strconv"
 
@@ -8,34 +9,40 @@ import (
 )
 
 const (
-	DEFAULT_PASSWORD_LENGTH int    = 32
-	END_OF_LINE             string = "\n"
+	DEFAULT_PASSWORD_LENGTH = 32
+	VERSION                 = "0.9.1"
 )
 
-func main() {
-	var (
-		passGen        = passgen.New()
-		passwordLength = getPasswordLength()
-		password       = passGen.Generate(passwordLength)
-	)
+var (
+	passGen           = passgen.New()
+	passwordLength    = DEFAULT_PASSWORD_LENGTH
+	isUserWantVersion = false
+)
 
-	os.Stdout.Write(password)
-	os.Stdout.WriteString(END_OF_LINE)
-}
-
-func getPasswordLength() int {
-	var (
-		args   = os.Args
-		length int
-	)
-
-	if len(args) > 1 {
-		length, _ = strconv.Atoi(args[1])
-
-		if length > 0 {
-			return length
-		}
+func init() {
+	if len(os.Args) == 1 {
+		return
 	}
 
-	return DEFAULT_PASSWORD_LENGTH
+	secondArg := os.Args[1]
+
+	if (secondArg == "-v") || (secondArg == "--version") {
+		isUserWantVersion = true
+		return
+	}
+
+	passLength, _ := strconv.Atoi(secondArg)
+
+	if passLength > 0 {
+		passwordLength = passLength
+	}
+}
+
+func main() {
+	if isUserWantVersion {
+		fmt.Println("Version:", VERSION)
+	} else {
+		password := passGen.Generate(passwordLength)
+		fmt.Println(string(password))
+	}
 }
